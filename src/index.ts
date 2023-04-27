a0db8c6695fcaeaa2325498213a271314b8ae5ec
Maignan Ben-ammi <maignan.benammi@gmail.com>
2023-04-27 10:40:56 +0200

    First commit

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const app = express();
 const PORT = Number(process.env.PORT) | 8801;
 
 app.get("/", (req, res) => {
-  res.send("Hello, world!");
+  res.send("Hello, world !!");
 });
 
 app.listen(PORT, () => {
